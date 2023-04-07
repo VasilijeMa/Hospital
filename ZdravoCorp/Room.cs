64bd1053c66c51a8b34ed5dbc9ba2e6446f10ecc
Vasilije Marković <markovic.sv15.2021@uns.ac.rs>
2023-04-07 14:27:04 +0200

    Created Room class and modified Equipment class.

---
+++ ZdravoCorp/Room.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Diagnostics.Eventing.Reader;
+using System.Windows.Documents;
+
+public enum RoomType { OperatingTheatre, ExaminationRoom, Infirmary, WaitingRoom}
+public class Room
+{
+	private string Name;
+	private RoomType Type;
+	private Dictionary<Equipment, int> Inventory;
+	public Room(int type, string name, Dictionary<Equipment, int> inventory)
+	{
+		this.Type = (RoomType)type;
+		this.Name = name;
+		this.Inventory = inventory;
+	}
+	void Add(Equipment equipment)
+	{
+		if (this.Inventory.ContainsKey(equipment))
+		{
+			this.Inventory[equipment]++;
+		}
+		else
+		{
+			this.Inventory.Add(equipment, 1);
+		}
+	}
+}

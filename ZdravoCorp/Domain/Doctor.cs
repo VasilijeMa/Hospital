e3a4b4b5f7085ba2ed784cd20817e6c907371b1a
Milos Bojanic <mbojanic53@gmail.com>
2023-05-23 18:15:46 +0200

    finished feature. Searching doctors by first and last name and specialization.

--- ZdravoCorp/Domain/Doctor.cs
+++ ZdravoCorp/Domain/Doctor.cs
@@ -12,7 +12,7 @@ namespace ZdravoCorp.Domain
         public string FirstName { get; set; }
         public string LastName { get; set; }
         public Specialization Specialization { get; set; }
-
+        public List<double> Ratings { get; set; }
         public Doctor() : base() { }
 
         public Doctor(int id, string name, string lastname, Specialization specialization, string username, string password, string type) : base(username, password, type)

90c5f062e002e2ea0f3244d5f828cc467f3ee534
Milos Bojanic <mbojanic53@gmail.com>
2023-05-25 19:49:50 +0200

    added classes for patient notifications

---
+++ ZdravoCorp/Core/Domain/Notification.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ZdravoCorp.Core.Domain
+{
+    public class Notification
+    {
+        public int Id { get; set; }
+        public string Title { get; set; }
+        public int TimesPerDay { get; set; }
+        public int MinutesBefore { get; set; }
+        public Notification(){}
+        public Notification(int id, string title, int timesPerDay, int minutesBefore)
+        {
+            Id = id;
+            Title = title;
+            TimesPerDay = timesPerDay;
+            MinutesBefore = minutesBefore;
+        }
+    }
+}
---
+++ ZdravoCorp/Core/Repositories/NotificationRepository.cs
@@ -0,0 +1,37 @@
+﻿using Microsoft.VisualBasic.ApplicationServices;
+using Newtonsoft.Json;
+using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Security.Permissions;
+using System.Text;
+using System.Threading.Tasks;
+using ZdravoCorp.Core.Domain;
+
+namespace ZdravoCorp.Core.Repositories
+{
+    public class NotificationRepository
+    {
+        private List<Notification> notifications;
+        public List<Notification> Notifications { get => notifications;}
+
+        public NotificationRepository()
+        {
+            notifications = LoadAll();
+        }
+        public List<Notification> LoadAll()
+        {
+            var serializer = new JsonSerializer();
+            using StreamReader reader = new("./../../../data/patientNotifications.json");
+            var json = reader.ReadToEnd();
+            return JsonConvert.DeserializeObject<List<Notification>>(json);
+        }
+        public void WriteAll()
+        {
+            string json = JsonConvert.SerializeObject(notifications, Formatting.Indented);
+            File.WriteAllText("./../../../data/patientNotifications.json", json);
+        }
+
+    }
+}

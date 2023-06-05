dfd981d643dbbf730ba7e9e494f86c670e6ccaff
Milos Bojanic <mbojanic53@gmail.com>
2023-06-06 01:59:47 +0200

    refactored MyAppointment class (mvvm), and connecti with doctor survey

---
+++ ZdravoCorp/Core/Commands/DoctorSurveyCommand.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using ZdravoCorp.GUI.View.Patient;
+using ZdravoCorp.GUI.ViewModel;
+
+namespace ZdravoCorp.Core.Commands
+{
+    public class DoctorSurveyCommand : BaseCommand
+    {
+        private MyAppointmentsViewModel viewModel;
+        public DoctorSurveyCommand(MyAppointmentsViewModel viewModel)
+        {
+            this.viewModel = viewModel;
+        }
+
+        public override bool CanExecute(object? parameter)
+        {
+            if(viewModel.SelectedAppointment == null || viewModel.SelectedAppointment.IsCanceled) return false;
+            if (viewModel.SelectedAppointment.TimeSlot.start >= DateTime.Now) return false;
+            return true;
+        }
+
+        public override void Execute(object? parameter)
+        {
+            DoctorSurveyView doctorSurveyView =
+                new DoctorSurveyView(viewModel.Patient, viewModel.SelectedAppointment.DoctorId);
+            doctorSurveyView.ShowDialog();
+        }
+    }
+}
--- ZdravoCorp/GUI/View/Patient/MyAppointmentsWindow.xaml.cs
+++ ZdravoCorp/GUI/View/Patient/MyAppointmentsWindow.xaml.cs
@@ -6,6 +6,7 @@ using System.Windows.Controls;
 using ZdravoCorp.Core.Domain;
 using ZdravoCorp.Core.Repositories;
 using ZdravoCorp.Core.Servieces;
+using ZdravoCorp.GUI.ViewModel;
 
 namespace ZdravoCorp
 {
@@ -14,112 +15,11 @@ namespace ZdravoCorp
     /// </summary>
     public partial class MyAppointmentsWindow : Window
     {
-        List<Appointment> appointments;
-        Patient patient;
         private ScheduleService scheduleService = new ScheduleService();
-        //private ScheduleRepository scheduleRepository;
         public MyAppointmentsWindow(Patient patient)
         {
             InitializeComponent();
-            this.patient = patient;
-            appointments = scheduleService.GetAppointmentsForPatient(patient.Id);
-            LoadAppointmentsInDataGrid();
-        }
-
-        private void LoadAppointmentsInDataGrid()
-        {
-            DataTable dt = new DataTable();
-            dt.Columns.Add("Id", typeof(int));
-            dt.Columns.Add("Date", typeof(string));
-            dt.Columns.Add("Time", typeof(string));
-            dt.Columns.Add("DoctorID", typeof(int));
-            dt.Columns.Add("IsCanceled", typeof(bool));
-            foreach (Appointment appointment in appointments)
-            {
-                dt.Rows.Add(appointment.Id, appointment.TimeSlot.start.Date.ToString("yyyy-MM-dd"), appointment.TimeSlot.start.TimeOfDay.ToString(), appointment.DoctorId, appointment.IsCanceled);
-            }
-            dgAppointments.ItemsSource = dt.DefaultView;
-        }
-
-        private void dgAppointments_SelectionChanged(object sender, SelectionChangedEventArgs e)
-        {
-            if (dgAppointments.SelectedItem == null)
-            {
-                return;
-            }
-            if (((DataRowView)dgAppointments.SelectedItem).Row["IsCanceled"].Equals(true))
-            {
-                btnCancel.IsEnabled = false;
-            }
-            else
-            {
-                btnCancel.IsEnabled = true;
-            }
-        }
-
-        private void btnCancel_Click(object sender, RoutedEventArgs e)
-        {
-            DataRowView item = dgAppointments.SelectedItem as DataRowView;
-            if (item == null)
-            {
-                MessageBox.Show("Appointment is not selected.");
-                return;
-            }
-            Appointment appointment = scheduleService.GetAppointmentById((int)item.Row["Id"]);
-            if (appointment.TimeSlot.start <= DateTime.Now.AddDays(1))
-            {
-                MessageBox.Show("The selected appointment must not changed.");
-                return;
-            }
-            MessageBoxResult result = MessageBox.Show("Are you sure you want to cancel the appointment?", "Congfirmation", MessageBoxButton.YesNo, MessageBoxImage.Question);
-            if (result == MessageBoxResult.Yes)
-            {
-                ((DataRowView)dgAppointments.SelectedItem).Row["IsCanceled"] = true;
-                btnCancel.IsEnabled = false;
-                scheduleService.CancelAppointment(appointment.Id);
-
-                LogService logService = new LogService();
-                logService.UpdateCancelElement(appointment, patient);
-                if (patient.IsBlocked)
-                {
-                    this.Close();
-                }
-            }
-        }
-
-        private void btnExit_Click(object sender, RoutedEventArgs e)
-        {
-            this.Close();
-        }
-
-        private void btnUpdate_Click(object sender, RoutedEventArgs e)
-        {
-            DataRowView item = dgAppointments.SelectedItem as DataRowView;
-            if (item == null)
-            {
-                MessageBox.Show("Appointment is not selected.");
-                return;
-            }
-            if (item.Row["IsCanceled"].Equals(true))
-            {
-                MessageBox.Show("Appointment is canceled.");
-                return;
-            }
-            int id = (int)item.Row["Id"];
-            Appointment appointment = scheduleService.GetAppointmentById(id);
-            if (appointment.TimeSlot.start <= DateTime.Now.AddDays(1))
-            {
-                MessageBox.Show("The selected appointment must not changed.");
-                return;
-            }
-            UpdateWindow updateWindow = new UpdateWindow(appointment, patient);
-            updateWindow.ShowDialog();
-            dgAppointments.ItemsSource = null;
-            LoadAppointmentsInDataGrid();
-            if (patient.IsBlocked)
-            {
-                this.Close();
-            }
+            DataContext = new MyAppointmentsViewModel(patient, this);
         }
 
         private void Window_Closing(object sender, System.ComponentModel.CancelEventArgs e)
